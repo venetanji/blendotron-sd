5b272b39402da79c981e9f063ec321df6bcc9807
Giovanni Lion <giovanni.lion@gmail.com>
2022-12-01 17:04:19 +0800

    added gpt3 support

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # blendotron-sd
 
+Add `OPENAI_API_KEY='sk-***'` with your api key to a `.env` file in the root of the project or set it as environmental variable.
+
 ## Edit batch_config.yaml
 
 Specify output directory and desired seeds.
@@ -8,11 +10,26 @@ outpath: emotionsets
 seeds: [42,892,234,2389]
 ```
 
-Specify as many templates as you like
+Specify which models to run the batch with:
 ```
-templates:
+img-models:
+  - sd 
+  - dalle
+
+text-models:
+  - gpt3
+```
+
+Specify as many templates as you like. For dalle and sd:
+```
+img-templates:
   - a {subjects} expressing the emotion {emotions}
   - a {contexts} expressing {emotions}
+```
+For gpt3:
+```
+text-templates:
+  - 'Write a story about a {contexts} expressing the emotion {emotions}:'
 ```
 
 Specify two (TODO: generalize to n > 2) lists for each token:
@@ -22,6 +39,6 @@ contexts:
   - person
 
 emotions: 
-  - Amusement
+  - amusement
   - ...
 ```
--- gen.py
+++ gen.py
@@ -14,22 +14,28 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 batch_config =  yaml.safe_load(open('batch_config.yaml','r'))
 
+def create_outfolder(template,models):
+    tokens = [v[1] for v in string.Formatter().parse(template) if v[1]]
+    default = {}
+    for idx, token in enumerate(tokens):
+        default[token] = f'T{idx}'
+    fn = template.format(**default)
+    outpath = Path(batch_config['outpath'])/"".join(i for i in fn if i not in "\/:*?<>|")
+    outpath.mkdir(exist_ok=True, parents=True)
+    for model in batch_config[models]: Path(outpath/model).mkdir(exist_ok=True)
+    return outpath, tokens
+
+
 for seed in tqdm(batch_config['seeds']):
     torch.manual_seed(seed)
     print(f'Using seed {seed}')
-    pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2", torch_dtype=torch.float16, revision="fp16")
-    pipe = pipe.to("cuda")
+    if 'sd' in batch_config['img-models']:
+        pipe = StableDiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-2", torch_dtype=torch.float16, revision="fp16")
+        pipe = pipe.to("cuda")
 
-    for template in batch_config['templates']:
-        tokens = [v[1] for v in string.Formatter().parse(template)]
-        default = {}
-        for idx, token in enumerate(tokens):
-            default[token] = f'T{idx}'
-
-        outpath = Path(batch_config['outpath'])/template.format(**default)
-        outpath.mkdir(exist_ok=True, parents=True)
-        for model in batch_config['models']: Path(outpath/model).mkdir(exist_ok=True) 
-        
+    for template in batch_config['img-templates']:
+        outpath, tokens = create_outfolder(template, 'img-models')
+        print(tokens)       
         for idx, itoken in enumerate(tokens):
             if idx + 1 < len(tokens):
                 jtoken = tokens[idx+1]
@@ -39,11 +45,34 @@ for seed in tqdm(batch_config['seeds']):
                         pformat[itoken] = tki
                         pformat[jtoken] = tkj
                         prompt = template.format(**pformat)
-                        if 'sd' in batch_config['models']:
+                        if 'sd' in batch_config['img-models']:
                             image = pipe(prompt).images[0]  
                             image.save(outpath/'sd'/f'sd-{prompt}-{str(seed)}.png')
-                        if 'dalle' in batch_config['models']:
+                        if 'dalle' in batch_config['img-models']:
                             image_resp = openai.Image.create(prompt=prompt, n=1, size="512x512")
                             url = image_resp['data'][0]['url']
                             urllib.request.urlretrieve(url, outpath / 'dalle' /f'dalle-{prompt}-{image_resp["created"]}.png') 
-    del pipe
+
+    for template in batch_config['text-templates']:
+        outpath, tokens = create_outfolder(template, 'text-models')
+        print(tokens)
+        for idx, itoken in enumerate(tokens):
+            if idx + 1 < len(tokens):
+                jtoken = tokens[idx+1]
+                for tki in tqdm(batch_config[itoken]):
+                    for tkj in tqdm(batch_config[jtoken]):
+                        pformat = {}
+                        pformat[itoken] = tki
+                        pformat[jtoken] = tkj
+                        prompt = template.format(**pformat)
+                        completion = openai.Completion.create(
+                            model="text-davinci-003",
+                            prompt=prompt,
+                            max_tokens=2000,
+                            temperature=0.5,
+                            presence_penalty=0.5,
+                            frequency_penalty=0.5
+                        )
+                        with open(outpath / 'gpt3' / "".join(i for i in prompt if i not in "\/:*?<>|"), 'w') as f:
+                            f.write(completion.choices[0].text)
+                            print(completion)
